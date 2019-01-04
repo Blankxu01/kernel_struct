91a284369e288276f2ce1048b28ee52bdd37a571
blank <626622492@qq.com>
2019-01-04 06:48:18 -0800

    add all

--- List.h
+++ List.h
@@ -1,1 +1,16 @@
-hello
+#ifndef LIST_H
+#define LIST_H
+#define 
+struct list_head{
+	struct list_head *prev;
+	struct list_head *next;
+	}
+void list_head_init();
+void list_add(struct list_head *head);
+void list_add_tail(struct list_head *head);
+void list_del(struct list_head *head);
+bool list_empty(struct list_head *head); 
+void list_insert();
+
+#endif	
+
